aaa96691d371265d2ea95814af0fc685ba3a7320
jakequade <j2k4@pm.me>
2025-01-05 12:25:41 +1100

    added types for workout

---
+++ types/Workout.tsx
@@ -0,0 +1,69 @@
+type ID = string;
+
+export type Workout = {
+  name: string;
+  note: string;
+  rating_feel: number;
+  rating_food: number;
+  rating_sleep: number;
+  rating_stress: number;
+
+  exercises: Exercise[];
+};
+
+type Exercise = {
+  id: ID;
+  order_placed_in_workout: number;
+  sets: ExerciseSet;
+
+  definition: ExerciseDefinition;
+};
+
+type ExerciseSet = CommonSetProps & {
+  order_placed_in_exercise: number;
+  exercise_id: ID;
+  drop_sets: DropSet | undefined;
+};
+
+type DropSet = CommonSetProps & {
+  order_placed_in_set: number;
+};
+
+type CommonSetProps = {
+  target_weight: number;
+  actual_weight: number;
+  weight_unit: WeightUnit;
+  actual_reps: number;
+  target_reps_min: number;
+  target_reps_max: number;
+  target_rpe: number;
+  comment: string;
+};
+
+enum WeightUnit {
+  Kilograms = "kg",
+  Pounds = "lb",
+}
+
+type ExerciseDefinition = {
+  id: ID;
+  primary_muscles: Muscle[];
+  secondary_muscles: Muscle[];
+  unilateral: boolean;
+
+  equipment: Equipment;
+};
+
+enum Equipment {
+  Barbell = "barbell",
+  Dumbell = "dumbell",
+  Machine = "machine",
+  Cardio = "cardio",
+}
+
+type Muscle = {
+  name: string;
+  description: string;
+  // todo: some unique identifier for displaying it on body diagram
+  // todo: muscle_group? for what purpose?
+};
